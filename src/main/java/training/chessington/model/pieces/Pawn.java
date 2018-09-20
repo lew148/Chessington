679a0bdf98e8e79a487c44654ecd3ddb81ae34c8
Lewis Reid <lewis.reid148@gmail.com>
2018-09-20 13:27:08 +0100

    Sorted Pawn move UP/Down

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -1,5 +1,6 @@
 package training.chessington.model.pieces;
 
+import org.apache.logging.log4j.core.pattern.AbstractStyleNameConverter;
 import training.chessington.model.Board;
 import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
@@ -8,6 +9,8 @@ import training.chessington.model.PlayerColour;
 import java.util.ArrayList;
 import java.util.List;
 
+import static training.chessington.model.PlayerColour.*;
+
 public class Pawn extends AbstractPiece {
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
@@ -15,6 +18,17 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        ArrayList<Move> moves = new ArrayList<>();
+
+        if (this.colour.equals(PlayerColour.WHITE)) {
+            Move moveUp1 = new Move(from, from.plus(-1, 0));
+            moves.add(moveUp1);
+            if
+        } else if (this.colour.equals(PlayerColour.BLACK)) {
+            Move moveDown1 = new Move(from, from.plus(1, 0));
+            moves.add(moveDown1);
+        }
+
+        return moves;
     }
 }
