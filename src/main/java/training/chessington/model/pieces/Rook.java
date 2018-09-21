9b11611501977115569dce82378b9030b8b0e723
Lewis Reid <lewis.reid148@gmail.com>
2018-09-21 10:48:49 +0100

    1st stages of Rook functionality

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -15,6 +15,27 @@ public class Rook extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        ArrayList<Move> moves = new ArrayList<>();
+        Move moveUp1 = new Move(from, from.plus(-1, 0));
+        Move moveUp2 = new Move(from, from.plus(-2, 0));
+        Move moveUp3 = new Move(from, from.plus(-3, 0));
+        Move moveUp4 = new Move(from, from.plus(-4, 0));
+        Move moveUp5 = new Move(from, from.plus(-5, 0));
+        Move moveUp6 = new Move(from, from.plus(-6, 0));
+        Move moveUp7 = new Move(from, from.plus(-7, 0));
+
+
+        if (board.get(from.plus(-1, 0)) == null) {
+            moves.add(moveUp1);
+            moves.add(moveUp2);
+            moves.add(moveUp3);
+            moves.add(moveUp4);
+            moves.add(moveUp5);
+            moves.add(moveUp6);
+            moves.add(moveUp7);
+        }
+
+        return moves;
     }
+
 }
--- src/test/java/training/chessington/model/pieces/RookTest.java
+++ src/test/java/training/chessington/model/pieces/RookTest.java
@@ -1,7 +1,35 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class RookTest {
+
+    @Test
+    public void whiteRookCanMoveFromBottomToTop() {
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7, 3);
+        board.placePiece(coords, rook);
+
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-4, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-5, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-6, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-7, 0)));
+    }
+
+
 }
